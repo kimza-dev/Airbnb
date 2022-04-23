b846f68b8854641dd702a2a6384fd3cd9e518c99
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-24 01:28:42 +0300

    add cities location

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,34 +3,39 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import bg from "../images/frontpagebg.png";
 import logo from "../images/airbnb.png";
-import { ConnectButton, Select } from "web3uikit";
+import { ConnectButton, Icon, Select, DatePicker, Input, Button } from "web3uikit";
+import { useState } from "react";
+
 
 const Home = () => {
+  const [checkIn, setCheckIn] = useState(new Date());
+  const [checkOut, setCheckOut] = useState(new Date());
+  const [destination, setDestination] = useState("New York");
+  const [guests, setGuests] = useState(2);
 
   return (
     <>
-    <div className="container" style={{ backgroundImage: `url(${bg})` }}>
-      <div className="containerGradient"></div>
-    </div>
-
-    <div className="topBanner">
-      <div>
-        <img className="logo" src={logo} alt="logo"></img>
+      <div className="container" style={{ backgroundImage: `url(${bg})` }}>
+        <div className="containerGradinet"></div>
       </div>
-      <div className="tabs">
-        <div className="selected">Places To Stay</div>
-        <div>Experiences</div>
-        <div>Online Experiences</div>
+      <div className="topBanner">
+        <div>
+          <img className="logo" src={logo} alt="logo"></img>
+        </div>
+        <div className="tabs">
+          <div className="selected">Places To Stay</div>
+          <div>Experiences</div>
+          <div>Online Experiences</div>
+        </div>
+        <div className="lrContainers">
+          <ConnectButton />
+        </div>
       </div>
-      <div className="lrContainers">
-        <ConnectButton />
-      </div>
-    </div>
-    <div className="tabContent">
-      <div className="searchFields">
-        <div className="inputs">
-          Location
-          <Select
+      <div className="tabContent">
+        <div className="searchFields">
+          <div className="inputs">
+            Location
+            <Select
               defaultOptionIndex={0}
               onChange={(data) => setDestination(data.label)}
               options={[
@@ -39,37 +44,89 @@ const Home = () => {
                   label: "Nairobi",
                 },
                 {
-                  id: "msa",
+                  id: "Msa",
                   label: "Mombasa",
                 },
                 {
-                  id: "Naks",
-                  label: "Nakuru",
+                  id: "Isl",
+                  label: "Isiolo",
                 },
                 {
                   id: "Eld",
                   label: "Eldoret",
                 },
                 {
-                  id: "isl",
-                  label: "isiolo",
+                  id: "Nmg",
+                  label: "Namanga",
                 },
                 {
-                  id: "kjd",
+                  id: "Kjd",
                   label: "Kajiado",
                 },
-                
+                {
+                  id: "Thk",
+                  label: "Thika",
+                },
+                {
+                  id: "Nyc",
+                  label: "Nanyuki",
+                },
+                {
+                  id: "naks",
+                  label: "Nakuru",
+                },
               ]}
             />
           </div>
-        <div className="vl"></div>
-        <div className="inputs">Check In</div>
-        <div className="vl"></div>
-        <div className="inputs">Check Out</div>
-        <div className="vl"></div>
-        <div className="inputs">Guests</div>
+          <div className="vl" />
+          <div className="inputs">
+            Check In
+            <DatePicker
+              id="CheckIn"
+              onChange={(event) => setCheckIn(event.date)}
+            />
+          </div>
+          <div className="vl" />
+          <div className="inputs">
+            Check Out
+            <DatePicker
+              id="CheckOut"
+              onChange={(event) => setCheckOut(event.date)}
+            />
+          </div>
+          <div className="vl" />
+          <div className="inputs">
+            Guests
+            <Input
+              value={2}
+              name="AddGuests"
+              type="number"
+              onChange={(event) => setGuests(Number(event.target.value))}
+            />
+          </div>
+          <Link to={"/rentals"} state={{
+            destination: destination,
+            checkIn: checkIn,
+            checkOut: checkOut,
+            guests: guests
+          }}>
+          <div className="searchButton">
+            <Icon fill="#ffffff" size={24} svg="search" />
+          </div>
+          </Link>
+        </div>
       </div>
-    </div>
+      <div className="randomLocation">
+        <div className="title">Feel Adventurous</div>
+        <div className="text">
+          Let us decide and discover new places to stay, live, work or just
+          relax.
+        </div>
+        <Button
+          text="Explore A Location"
+          onClick={() => console.log(checkOut)}
+        />
+      </div>
     </>
   );
 };
