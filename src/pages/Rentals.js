d129f3587cef403e0bd837e6494a693abf26b8c5
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-25 20:37:46 +0300

    add Rentals file

--- src/pages/Rentals.js
+++ src/pages/Rentals.js
@@ -2,13 +2,29 @@ import React from "react";
 import "./Rentals.css";
 import { Link } from "react-router-dom";
 import logo from "../images/airbnbRed.png";
-import { ConnectButton, Icon} from "web3uikit";
+import { ConnectButton, Icon, Button } from "web3uikit";
 import { useLocation } from "web3uikit/node_modules/react-router";
 
 const Rentals = () => {
   const { state: searchFilters } = useLocation();
+  const rentalsList = [
+    {
+      attributes: {
+        city: "Nairobi",
+        unoDescription: "3 Guests • 2 Beds • 2 Rooms",
+        dosDescription: "Wifi • Kitchen • Living Area",
+        imgurl: "https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/3",
+        lat: "",
+        long: "",
+        name: "Appartment in CBD",
+        pricePerDay_decimal: "3"
+      }
+    }
+  ];
+
   return (
     <>
+    <hr className="line"/>
      <div className="topBanner">
         <div>
           <Link to="/">
