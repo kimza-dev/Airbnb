6496f3a1ee28b5a8272851c3cb3100b5f6f60110
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-25 21:28:54 +0300

    add map

--- src/components/RentalsMap.js
+++ src/components/RentalsMap.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { Map, Market, GoogleApiWrapper } from "google-maps-react";
+import { useState, useEffect} from "react";
 
-function RentalsMap() {
+
+function RentalsMap(locations) {
+  const [center, seCenter] = useState();
+  useEffect(() => {
+    var arr = Object.keys(locations);
+    var getLat = (key) => locations[key]["lat"];
+    var avgLat = arr.reduce((a, c) => a = Number(getLat(c)), 0) / arr.length;
+
+    var getLng = (key) => locations[key]["lng"];
+    var avgLng = arr.reduce((a, c) => a = Number(getLng(c)), 0) / arr.length;
+
+    seCenter({lat:avgLat, lng:avgLng })
+    
+  }, [locations])
 
   return (
     <>
@@ -9,4 +24,7 @@ function RentalsMap() {
   );
 }
 
-export default RentalsMap;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBtiJe6xqasylmoGptTGNVG-ORTBBUQKZc"
+})
+(RentalsMap);
