81dbf8d1808b6955dd14c1716efb4b8d50f63c1e
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-25 20:24:38 +0300

    add Rentals file

--- src/pages/Rentals.js
+++ src/pages/Rentals.js
@@ -1,36 +1,17 @@
 import React from "react";
 import "./Rentals.css";
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router";
+import { link } from "react-router-dom";
+import { uselocation } from "react-router";
 import logo from "../images/airbnbRed.png";
-import { ConnectButton, Icon, Button } from "web3uikit";
+import { ConnectButton, Icon, Button, useNotification } from "web3uikit";
 import RentalsMap from "../components/RentalsMap";
+import { useLocation } from "web3uikit/node_modules/react-router";
 
 const Rentals = () => {
   const { state: searchFilters } = useLocation();
-  const rentalsList = [
-     {
-        attributes: {
-          city: "New York",
-          unoDescription: "3 Guests • 2 Beds • 2 Rooms",
-          dosDescription: "Wifi • Kitchen • Living Area",
-          imgUrl: "https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/3",
-          lat: "40.716862",
-          long: "-73.999005",
-          name: "Apartment in China Town",
-          pricePerDay_decimal: "3"
-        }
-     }
-  ];
-
-  let cords = [];
-  rentalList.forEach((e) => {
-    cords.push({ lat: e.attributes.lat, lng: e.attributes.long });
-  });
-
   return (
     <>
-      <div className="topBanner">
+     <div className="topBanner">
         <div>
           <Link to="/">
             <img className="logo" src={logo} alt="logo"></img>
@@ -66,44 +47,7 @@ const Rentals = () => {
           <ConnectButton />
         </div>
       </div>
-      <hr className="line" />
-      <div className="rentalsContent">
-        <div className="rentalsContentL">
-          Stays Available For Your Destination
-          {rentalsList &&
-            rentalsList.map((e, i) => {
-              return (
-                <>
-                  <hr className="line2" />
-                  <div className="rentalDiv">
-                    <img className="rentalImg" src={e.attributes.imgUrl}></img>
-                    <div className="rentalInfo">
-                      <div className="rentalTitle">{e.attributes.name}</div>
-                      <div className="rentalDesc">
-                        {e.attributes.unoDescription}
-                      </div>
-                      <div className="rentalDesc">
-                        {e.attributes.dosDescription}
-                      </div>
-                      <div className="bottomButton">
-                        <Button
-                          text="Stay Here"
-                        />
-                        <div className="price">
-                          <Icon fill="#808080" size={10} svg="matic" />{" "}
-                          {e.attributes.pricePerDay} / Day
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-        </div>
-        <div className="rentalsContentR">
-          <RentalsMap locations={cords} />
-        </div>
-     </>
+    </>
   );
 };
 
