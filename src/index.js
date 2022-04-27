fbf891eaf1a353f5cb329809158e240081db9bbc
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-27 18:29:46 +0300

    rentals

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { NotificationProvider } from 'web3uikit';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId="xxxx" serverUrl="xxxx">
+    <MoralisProvider appId="ZALu8mLvWV3LbnCnfTwIvVBr7ge08PGPftHShx39" serverUrl="https://n1q6e5glblly.usemoralis.com:2053/server">
       <NotificationProvider>
       <BrowserRouter>
         <App />
